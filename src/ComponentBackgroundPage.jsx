c96600044ae26afc9091c13b8851e1adf200081d
Marlene Angouillant <marlene.angouillant@gmail.com>
2019-04-18 22:18:12 +0200

    linkApi

--- src/ComponentBackgroundPage.jsx
+++ src/ComponentBackgroundPage.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import Title from './Title';
-import ImageEggHome from './ImageEggHome';
-import ButtonStart from './ButtonStart';
-import styles from './componentbackground.module.css';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Title from "./Title";
+import ButtonStart from "./ButtonStart";
+import styles from "./componentbackground.module.css";
+import LinkApi from "./ComponentLinkAPI";
 
 const ComponentBackgroundPage = () => {
 	return (
 		<div>
-			{/* <div className={styles.BackgroundHome}>  */}
-			<ImageEggHome />
+			<LinkApi />
 			<Title />
 			<ButtonStart />
 		</div>
