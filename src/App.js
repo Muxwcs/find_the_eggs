9a68b7ab222e3c4b9ba0fee63a71393d6ca6583a
Stéphane Vivier <contact.xsprod@gmail.com>
2019-04-18 22:58:15 +0200

    Added a merdic code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import ComponentBackgroundPage from './ComponentBackgroundPage';
+import NameForm from './NameForm';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
 				<ComponentBackgroundPage />
+				<NameForm />
 			</div>
 		);
 	}
