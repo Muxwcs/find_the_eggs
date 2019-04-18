79f3783394d94de570da2b340e8769f9a73049bc
Cloe Truel <cloe.truel@gmail.com>
2019-04-18 16:18:46 +0200

    image card for home

---
+++ src/ImageEggHome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card, CardImg } from "reactstrap";
+
+export default function ImageEgg() {
+	return (
+		<div>
+			<Card>
+				<CardImg
+					width="100%"
+					src="https://www.e-scrap.fr/3265-7302-large_default/stickers-deco-oeuf-paques-rose-scrapbooking.jpg"
+					alt="Happy Easter"
+				/>
+			</Card>
+		</div>
+	);
+}
