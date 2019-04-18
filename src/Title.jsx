0a9e7feff04d87debd32af90642a6c007bfd0fc6
Guillaume Ribéroux <guillaume.riberoux@gmail.com>
2019-04-18 16:15:57 +0200

    First commit title

---
+++ src/Title.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Container,
+    Row,
+    Col,
+}
+    from 'reactstrap';
+
+class Title extends Component {
+    render() {
+        return ( 
+            <div>
+                <h1>HAPPY EASTER YOU</h1>
+            </div>
+         );
+    }
+}
+
+export default Title;
