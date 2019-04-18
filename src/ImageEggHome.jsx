493196c0b38983643d8c388e122b7579b637f3bf
Stéphane Vivier <contact.xsprod@gmail.com>
2019-04-18 17:34:56 +0200

    Added a home page component

--- src/ImageEggHome.jsx
+++ src/ImageEggHome.jsx
@@ -1,16 +1,11 @@
-import React from "react";
-import { Card, CardImg } from "reactstrap";
+import React from 'react';
+import { Card, CardImg } from 'reactstrap';
+import easteregg from '../src/img/easteregg.png';
 
 export default function ImageEgg() {
 	return (
 		<div>
-			<Card>
-				<CardImg
-					width="100%"
-					src="https://www.e-scrap.fr/3265-7302-large_default/stickers-deco-oeuf-paques-rose-scrapbooking.jpg"
-					alt="Happy Easter"
-				/>
-			</Card>
+			<img src={easteregg} />
 		</div>
 	);
 }
