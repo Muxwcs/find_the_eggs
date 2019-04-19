b58be2e6afebb7b3ef2a3c70cd0076f6cdf3c70b
Marlene Angouillant <marlene.angouillant@gmail.com>
2019-04-19 02:37:50 +0200

    create Routeur component with route link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-
-import ComponentBackgroundPage from "./ComponentBackgroundPage";
+import Routeur from "./Component/Routeur";
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<ComponentBackgroundPage />
+				<Routeur />
 			</div>
 		);
 	}
