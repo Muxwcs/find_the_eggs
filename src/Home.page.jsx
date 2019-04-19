f5870619f82ab67d5e15d0810feceb809c89f568
Sylene Manusset <sylene.manusset@gmail.com>
2019-04-19 09:24:52 +0200

    Removed the usage of body in css to avoid duplicate background in each page + fixed usage of module.css

--- src/Home.page.jsx
+++ src/Home.page.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Container } from "reactstrap";
-import "./home.module.css";
+import styles from "./home.module.css";
 import ModalStart from "./Component/ModalStart";
 import Title from "./Component/Title";
 import HomeLinkApi from "./Component/HomeLinkAPI";
 
 const Home = () => {
-	return (
-		<Container className='Bghome' style={{ height: "100vh", textAlign: "center" }}>
-			<HomeLinkApi />
-			<Title />
-			<ModalStart />
-		</Container>
-	);
+    return (
+        <Container className={styles.bghome} fluid>
+            <HomeLinkApi />
+            <Title />
+            <ModalStart />
+        </Container>
+    );
 };
 
 export default Home;
